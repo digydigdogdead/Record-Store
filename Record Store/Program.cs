1232da20ec151cc362599c66897856783229895d
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-14 12:03:28 +0000

    Implement in-memory server for testing

--- Record Store/Program.cs
+++ Record Store/Program.cs
@@ -1,7 +1,9 @@
 
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Record_Store.Models;
 using Record_Store.Services;
+using System;
 
 namespace Record_Store
 {
@@ -11,9 +13,9 @@ namespace Record_Store
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            string connectionString = builder.Configuration.GetConnectionString("Connection");
-            builder.Services.AddDbContext<RecordStoreDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            builder.Services.AddDbContext<RecordStoreDbContext>(o => o.UseSqlite(connection));
 
             // Add services to the container.
 
@@ -25,6 +27,7 @@ namespace Record_Store
             builder.Services.AddScoped<IAlbumService, AlbumService>();
 
             var app = builder.Build();
+            AddAlbumData(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -41,6 +44,37 @@ namespace Record_Store
             app.MapControllers();
 
             app.Run();
+        }
+
+        static void AddAlbumData(WebApplication app)
+        {
+            var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetService<RecordStoreDbContext>();
+
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            var takeMeBackToEden = new Album
+            {
+                Id = 1,
+                Artist = "Sleep Token",
+                Name = "Take Me Back To Eden",
+                Year = 2023
+            };
+
+            var madagascar2 = new Album
+            {
+                Id = 2,
+                Artist = "Various Artists",
+                Name = "Madagascar: Escape 2 Africa",
+                Year = 2008
+            };
+
+            db.Albums.Add(takeMeBackToEden);
+            db.Albums.Add(madagascar2);
+            
+            db.SaveChanges();
         }
     }
 }
