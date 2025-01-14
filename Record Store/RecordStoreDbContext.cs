bdb294403e7a9eb534195f04e745003a39be35ef
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-14 10:12:18 +0000

    Create DbContext

---
+++ Record Store/RecordStoreDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Record_Store
+{
+    public class RecordStoreDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        public RecordStoreDbContext(DbContextOptions<RecordStoreDbContext> options) : base(options) { }
+    }
+}
