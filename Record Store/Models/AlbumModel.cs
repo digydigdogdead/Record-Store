0f7ed17b03d88c426a699b3d846ec0b4c34a9a5f
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-14 15:04:51 +0000

    Add TryPostAlbum at model layer

--- Record Store/Models/AlbumModel.cs
+++ Record Store/Models/AlbumModel.cs
@@ -24,5 +24,29 @@
         {
             return _db.Albums.FirstOrDefault(a => a.Id == id);
         }
+
+        public bool TryPostAlbum(Album album, out string feedback)
+        {
+            var allAlbums = GetAllAlbums();
+
+            if (allAlbums.Any(a => a.Id == album.Id))
+            {
+                feedback = "The supplied Id is not unique.";
+                return false;
+            }
+
+            try
+            {
+                _db.Albums.Add(album);
+                _db.SaveChanges();
+                feedback = "Success";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                feedback = ex.Message;
+                return false;
+            }
+        }
     }
 }
