08b331c3a9221973e2872bf2c97c5fd3319636bb
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-27 16:52:15 +0000

    Add functionality to AddAlbum page to make Post requests

--- Record Store/Data Classes/Album.cs
+++ Record Store/Data Classes/Album.cs
@@ -1,11 +1,17 @@
-﻿namespace Record_Store
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Record_Store
 {
     public class Album
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Artist { get; set; }
+        [Required, Range(1920, 2026)]
         public int Year { get; set; }
+        [Required]
         public ParentGenre ParentGenre { get; set; }
         public string? Subgenre { get; set; }
         public int Stock { get; set; } = 1;
