0004730ca43e91c23ea6d8a6cdb6267f0d452a8c
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-27 16:57:32 +0000

    Add year validation to AlbumDTO to be used with RecordDetails component

--- Record Store/Data Classes/AlbumDTO.cs
+++ Record Store/Data Classes/AlbumDTO.cs
@@ -1,10 +1,14 @@
-﻿namespace Record_Store
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Record_Store
 {
     public class AlbumDTO
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
         public string? Artist { get; set; }
+        [Range(1920, 2026)]
         public int? Year { get; set; }
         public ParentGenre? ParentGenre { get; set; }
         public string? Subgenre { get; set; }
