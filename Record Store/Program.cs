c632b6ffa0b700c1f41618da93e18d87008fb800
digydigdogdead <carlyle.laurent@gmail.com>
2025-02-20 12:14:36 +0000

    Abstract seeding initial data into its own class rather than cluttering Program.cs

--- Record Store/Program.cs
+++ Record Store/Program.cs
@@ -33,7 +33,7 @@ namespace Record_Store
             builder.Services.AddScoped<IAlbumService, AlbumService>();
 
             var app = builder.Build();
-            AddAlbumData(app);
+            SeedData.AddAlbumData(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -53,39 +53,6 @@ namespace Record_Store
             app.Run();
         }
 
-        static void AddAlbumData(WebApplication app)
-        {
-            var scope = app.Services.CreateScope();
-            var db = scope.ServiceProvider.GetService<RecordStoreDbContext>();
-
-
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var takeMeBackToEden = new Album
-            {
-                Id = 1,
-                Artist = "Sleep Token",
-                Name = "Take Me Back To Eden",
-                Year = 2023,
-                ParentGenre = ParentGenre.METAL,
-                Stock = 1
-            };
-
-            var madagascar2 = new Album
-            {
-                Id = 2,
-                Artist = "Various Artists",
-                Name = "Madagascar: Escape 2 Africa",
-                Year = 2008,
-                ParentGenre = ParentGenre.CLASSICAL,
-                Stock = 1
-            };
-
-            db.Albums.Add(takeMeBackToEden);
-            db.Albums.Add(madagascar2);
-            
-            db.SaveChanges();
-        }
+        
     }
 }
---
+++ Record Store/SeedData.cs
@@ -0,0 +1,40 @@
+﻿namespace Record_Store
+{
+    public static class SeedData
+    {
+        public static void AddAlbumData(WebApplication app)
+        {
+            var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetService<RecordStoreDbContext>();
+
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            var takeMeBackToEden = new Album
+            {
+                Id = 1,
+                Artist = "Sleep Token",
+                Name = "Take Me Back To Eden",
+                Year = 2023,
+                ParentGenre = ParentGenre.METAL,
+                Stock = 1
+            };
+
+            var madagascar2 = new Album
+            {
+                Id = 2,
+                Artist = "Various Artists",
+                Name = "Madagascar: Escape 2 Africa",
+                Year = 2008,
+                ParentGenre = ParentGenre.CLASSICAL,
+                Stock = 1
+            };
+
+            db.Albums.Add(takeMeBackToEden);
+            db.Albums.Add(madagascar2);
+
+            db.SaveChanges();
+        }
+    }
+}
